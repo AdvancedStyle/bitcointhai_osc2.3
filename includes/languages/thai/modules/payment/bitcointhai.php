663f3cc3b663b7a3f7313d2c626ddc68560339c2
David Barnes <dave@advancedstyle.com>
2013-05-04 21:19:21 +0700

    Thai language file
    Added Thai language definitions (needs to be translated)

--- includes/languages/thai/modules/payment/bitcointhai.php
+++ includes/languages/thai/modules/payment/bitcointhai.php
@@ -12,4 +12,12 @@
 
   define('MODULE_PAYMENT_BITCOINTHAI_TEXT_TITLE', 'Bitcoin');
   define('MODULE_PAYMENT_BITCOINTHAI_TEXT_DESCRIPTION', 'Bitcoin');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_PAYMSG','You must send <strong>%s</strong> Bitcoins to the address: %s');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_AFTERPAY','After you have completed payment please click the '.IMAGE_BUTTON_CONFIRM_ORDER.' button');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_ADDRESS','Bitcoin Address:');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_AMOUNT','Bitcoin Amount:');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_COUNTDOWN','You must send the bitcoins within the next %s Minutes %s Seconds');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_COUNTDOWN_EXP','Bitcoin payment time has expired, please refresh the page to get a new address');
+  define('MODULE_PAYMENT_BITCOINTHAI_TEXT_ERROR','Sorry Bitcoin payments are currently unavailable');
+  define('MODULE_PAYMENT_BITCOINTHAI_TITLE_ERROR','Bitcoin Error');
 ?>
